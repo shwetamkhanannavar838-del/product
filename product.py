3d8b8ced450161d6f20ad7af32f807a8623317b0
Shweta.khanannavar <shwetamkhanannavar838@gmail.com>
2025-11-30 19:24:30 +0530

    Product Details

---
+++ product.py
@@ -0,0 +1,11 @@
+def get_product_details(prd_id,name,quantity,price):
+   return(
+      f"Product ID: {prd_id}\n"
+      f"Product name: {name}\n"
+      f"Quantity: {quantity}\n"
+      f"Price: {price}" 
+  )
+
+#Example usage (optional)
+if __name__=="__main__":
+    print(get_product_details("P101","Wireless Mouse",2,550.00))
